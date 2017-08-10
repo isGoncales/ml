62238420d342cf945dc5a506b3955d25553d7030
Ivan Seguro Gonçales <ivangoncales@yahoo.com.br>
2017-08-09 22:57:08 -0300

    Incluindo css

--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,17 @@ var router = express.Router();
 var https = require('https');
 
 /* GET users listing. */
-//router.get('/items', function(req, res, next) {
-//res.send('Carrega tela de resultados para a busca: ' + req.query.search);
-//res.render('results', { title: 'Carrega tela de resultados para a busca: ' + req.query.search});
-//});
 router.get('/items', function(req, res, next) {
   var url = 'https://api.mercadolibre.com/sites/MLA/search?q=:' + req.query['search'] + '&limit=4';
-  //console.log(url);
   https.get(url, (response) => {
     var data = "";
-    //console.log('statusCode:', res.statusCode);
-    //console.log('headers:', res.headers);
     response.on('data', (d) => {
       data += d;
     });
 
     response.on('end', function() {
-      //console.log(data);
       res.render('results', {
-        title: 'Carrega tela de resultados para a busca: ' + req.query['search'],
+        title: 'Teste prático Mercado Livre - Resultados',
         data: JSON.parse(data)
       });
     });
@@ -41,23 +33,15 @@ router.get('/items/:id', function(req, res, next) {
     });
 
     response.on('end', function() {
-      //console.log(data);
       res.render('detail', {
-        title: 'Carrega tela de detalhe do produto ID: ' + req.params.id,
+        title: 'Teste prático Mercado Livre - Detalhe',
         data: JSON.parse(data)
-      //loadDescription(data, req.params.id, res);
       });
     });
 
   }).on('error', (e) => {
     console.error(e);
   });
-
-  //res.render('detail', {
-  //  title: 'Carrega tela de detalhe do produto ID: ' + req.params['id']
-  //});
 });
-
-//var url = 'https://api.mercadolibre.com/items/' + id + '/description';
 
 module.exports = router;
